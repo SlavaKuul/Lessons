e75f53b658ca34d83a611cfd7f8a597833fd5dcd
Slava <s.pokoyanov@mail.ru>
2025-09-15 21:39:10 +0700

    Исправлены аннотации типов и имена переменных

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
-def filter_by_state(date_list: list, state = 'EXECUTED') -> list:
-    """Функция сортировки списка по статусу"""
-    sorted_list = []
+def filter_by_state(date_list: list[dict[str, any]], state: str = 'EXECUTED') -> list[dict[str, any]]:
+    """Функция фильтрации списка по статусу"""
+    filtered_list = []
     for current_id in date_list:
         id_state = current_id.get('state')
         if id_state == state:
-            sorted_list.append(current_id)
-    return sorted_list
+            filtered_list.append(current_id)
+    return filtered_list
 
 
-def sort_by_date(date_list: list, sort_order = 'True') -> list:
+def sort_by_date(date_list: list[dict[str, any]], sort_order: str = 'True') -> list[dict[str, any]]:
     """Функция сортировки списка по дате"""
     if sort_order == 'True':
         sorted_list = sorted(date_list, key=lambda x: x['date'], reverse = True)
