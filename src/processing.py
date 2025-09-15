a02c845821984879ce9d6fb03914265354cc638c
Slava <s.pokoyanov@mail.ru>
2025-09-15 18:32:10 +0700

    Добавлена функция сортировки списка по дате

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,13 @@ def filter_by_state(date_list: list, state = 'EXECUTED') -> list:
         if id_state == state:
             sorted_list.append(current_id)
     return sorted_list
+
+
+def sort_by_date(date_list: list, sort_order = 'True') -> list:
+    """Функция сортировки списка по дате"""
+    if sort_order == 'True':
+        sorted_list = sorted(date_list, key=lambda x: x['date'], reverse = True)
+    else:
+        sorted_list = sorted(date_list, key=lambda x: x['date'])
+    return sorted_list
+
